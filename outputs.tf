23846f70d5afa25f98a8449e7d605e9c604e998c
Daniel Allen <dan@aeternagroup.net>
2026-01-11 22:17:59 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "virtual_hub_route_tables" {
+  description = "All virtual_hub_route_table resources"
+  value       = azurerm_virtual_hub_route_table.virtual_hub_route_tables
+}
+
---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "virtual_hub_route_tables" {
+  description = <<EOT
+Map of virtual_hub_route_tables, attributes below
+Required:
+    - name
+    - virtual_hub_id
+Optional:
+    - labels
+    - route (block):
+        - destinations (required)
+        - destinations_type (required)
+        - name (required)
+        - next_hop (required)
+        - next_hop_type (optional)
+EOT
+
+  type = map(object({
+    name           = string
+    virtual_hub_id = string
+    labels         = optional(set(string))
+    route = optional(object({
+      destinations      = set(string)
+      destinations_type = string
+      name              = string
+      next_hop          = string
+      next_hop_type     = optional(string, "ResourceId")
+    }))
+  }))
+}
+
