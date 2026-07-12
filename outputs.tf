0118472588d5dd55b7737b78971c28a55c7242fd
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:32:34 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "virtual_hub_route_tables_id" {
+  description = "Map of id values across all virtual_hub_route_tables, keyed the same as var.virtual_hub_route_tables"
+  value       = { for k, v in azurerm_virtual_hub_route_table.virtual_hub_route_tables : k => v.id }
+}
 output "virtual_hub_route_tables_labels" {
   description = "Map of labels values across all virtual_hub_route_tables, keyed the same as var.virtual_hub_route_tables"
   value       = { for k, v in azurerm_virtual_hub_route_table.virtual_hub_route_tables : k => v.labels }
