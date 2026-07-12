6b34dca6f29ef5fa675d2b49f715265560a7b942
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:20 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "virtual_hub_route_tables" {
-  description = "All virtual_hub_route_table resources"
-  value       = azurerm_virtual_hub_route_table.virtual_hub_route_tables
-}
 output "virtual_hub_route_tables_labels" {
-  description = "List of labels values across all virtual_hub_route_tables"
-  value       = [for k, v in azurerm_virtual_hub_route_table.virtual_hub_route_tables : v.labels]
+  description = "Map of labels values across all virtual_hub_route_tables, keyed the same as var.virtual_hub_route_tables"
+  value       = { for k, v in azurerm_virtual_hub_route_table.virtual_hub_route_tables : k => v.labels }
 }
 output "virtual_hub_route_tables_name" {
-  description = "List of name values across all virtual_hub_route_tables"
-  value       = [for k, v in azurerm_virtual_hub_route_table.virtual_hub_route_tables : v.name]
+  description = "Map of name values across all virtual_hub_route_tables, keyed the same as var.virtual_hub_route_tables"
+  value       = { for k, v in azurerm_virtual_hub_route_table.virtual_hub_route_tables : k => v.name }
 }
 output "virtual_hub_route_tables_route" {
-  description = "List of route values across all virtual_hub_route_tables"
-  value       = [for k, v in azurerm_virtual_hub_route_table.virtual_hub_route_tables : v.route]
+  description = "Map of route values across all virtual_hub_route_tables, keyed the same as var.virtual_hub_route_tables"
+  value       = { for k, v in azurerm_virtual_hub_route_table.virtual_hub_route_tables : k => v.route }
 }
 output "virtual_hub_route_tables_virtual_hub_id" {
-  description = "List of virtual_hub_id values across all virtual_hub_route_tables"
-  value       = [for k, v in azurerm_virtual_hub_route_table.virtual_hub_route_tables : v.virtual_hub_id]
+  description = "Map of virtual_hub_id values across all virtual_hub_route_tables, keyed the same as var.virtual_hub_route_tables"
+  value       = { for k, v in azurerm_virtual_hub_route_table.virtual_hub_route_tables : k => v.virtual_hub_id }
 }
 
